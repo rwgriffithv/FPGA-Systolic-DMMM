4b646d2153a8142372e27a6a182fa0e78e25344b
Cloud User <centos@ip-172-31-27-11.ec2.internal>
2019-12-16 01:04:10 +0000

    moved stack allocation to heap allocation when possible

--- dmmm.hpp
+++ dmmm.hpp
@@ -39,26 +39,26 @@ public:
     cl::Kernel kernel(program, kernel_name.c_str());
     m_krnl_dmmm = new cl::KernelFunctor<cl::Buffer&, cl::Buffer&, cl::Buffer&, bool, unsigned int>(kernel);
 
-    std::vector<cl::Memory> inBufVec;
+    std::vector<cl::Memory> wt_p_bufs_vec;
     m_wt_ps = (float**)malloc(NUM_W_P * sizeof(float*));
     if (!m_wt_ps) {
-      std::cerr << "ERROR: failed to allocate memory for weight transpose partition pointers in DMMM\n";
+      std::cerr << "ERROR: failed to allocate memory for weight transpose partition pointers in DMMM constructor\n";
       exit(1);
     }
     m_wt_p_bufs = (cl::Buffer**)malloc(NUM_W_P * sizeof(cl::Buffer*));
     if (!m_wt_p_bufs) {
-      std::cerr << "ERROR: failed to allocate memory for weight transpose partition buffer pointers in DMMM\n";
+      std::cerr << "ERROR: failed to allocate memory for weight transpose partition buffer pointers in DMMM constructor\n";
       exit(1);
     }
 
-    // create transpose partitions of weight matrix w
+    // create transpose partitions of weight matrix w and associated OpenCL buffers
     for (size_t c = 0; c < NUM_C_P; ++c) {
       for (size_t f = 0; f < NUM_F_P; ++f) {
         
         // create F_P * C_P transpose partition wt_p
-        float* wt_p = (float*) malloc(SIZE_W_P * sizeof(float));
+        float* wt_p = (float*)malloc(SIZE_W_P * sizeof(float));
 	if (!wt_p) {
-	  std::cerr << "ERROR: failed to allocate memory for weight transpose partition in DMMM\n";
+	  std::cerr << "ERROR: failed to allocate memory for weight transpose partition in DMMM constructor\n";
 	  exit(1);
 	}
         
@@ -78,29 +78,52 @@ public:
                                             SIZE_W_P * sizeof(float), wt_p);
         m_wt_p_bufs[c * NUM_F_P + f] = w_p_buf; // keep track to use in kernel calls, indexed normally
 
-        inBufVec.push_back(*w_p_buf); // used for enqueing buffers to global memory
+        wt_p_bufs_vec.push_back(*w_p_buf); // used for enqueing buffers to global memory
       }
     }
 
-    //Copy weight input data to device global memory
-    m_queue->enqueueMigrateMemObjects(inBufVec, 0); // 0 means from host
+    // copy weight input data to device global memory
+    m_queue->enqueueMigrateMemObjects(wt_p_bufs_vec, 0); // 0 means from host
     m_queue->finish();
 
+    // allocate memory for layer partition, values will change depending on operate_row input
+    m_h_p = (float*)malloc(SIZE_H_P * sizeof(float));
+    if (!m_h_p) {
+      std::cerr << "ERROR: failed to allocate memory for layer partition in DMMM constructor\n";
+      exit(1);
+    }
+    m_h_p_buf = new cl::Buffer(*m_context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_WRITE,
+			       SIZE_H_P * sizeof(float), m_h_p);
+    m_h_p_mem.push_back(*m_h_p_buf);
+
+    // allocate memory for product of layer partition and weight partition
+    m_hw_p = (float*)malloc(SIZE_HW_P * sizeof(float));
+    if (!m_hw_p) {
+      std::cerr << "ERROR: failed to allocate memory for layer-weight partition product in DMMM constructor\n";
+      exit(1);
+    }
     m_hw_p_buf = new cl::Buffer(*m_context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_WRITE,
-            			     SIZE_HW_P * sizeof(float), m_hw_p);
-    m_outBufVec.push_back(*m_hw_p_buf);
+				SIZE_HW_P * sizeof(float), m_hw_p);
+    m_hw_p_mem.push_back(*m_hw_p_buf);
   }
 
   ~DMMM() {
+    // OpenCL setup
     delete m_context;
     delete m_queue;
     delete m_krnl_dmmm;
+    // weights
     for (size_t i = 0; i < NUM_W_P; ++i) {
       free(m_wt_ps[i]);
       delete m_wt_p_bufs[i];
     }
     free(m_wt_ps);
     free(m_wt_p_bufs);
+    // layer
+    free(m_h_p);
+    delete m_h_p_buf;
+    // product
+    free(m_hw_p);
     delete m_hw_p_buf;
   }
 
@@ -118,30 +141,22 @@ public:
       }
     }
     
-    float h_p[SIZE_H_P]; // current working partition of layer matrix h loaded in systolic array
-    std::vector<cl::Memory> inBufVec;
-
     // operate on each N_P * C_P partition
     for (size_t c = 0; c < NUM_C_P; ++c) {
-      // partition h
+      // partition h into m_h_p, already allocated space on heap
       for (size_t i = 0, h_i = H_R_ID * N_P; i < N_P; ++i, ++h_i) {
         for (size_t j = 0, h_j = c * C_P; j < C_P; ++j, ++h_j) {
           if ((h_i >= N) || (h_j >= C)) {
             // zero padding to fit in systolic array size
-            h_p[i*C_P + j] = 0.0;
+            m_h_p[i*C_P + j] = 0.0;
           } else {
-            h_p[i*C_P + j] = h_r[i * C + h_j];
+            m_h_p[i*C_P + j] = h_r[i * C + h_j];
           }
         }
       }
 
-      //Allocate Buffer in Global Memory
-      cl::Buffer h_p_buf(*m_context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-		                    SIZE_H_P * sizeof(float), h_p);
-      inBufVec.push_back(h_p_buf);
-
       //Copy layer partition data to device global memory
-      m_queue->enqueueMigrateMemObjects(inBufVec, 0); // 0 means from host
+      m_queue->enqueueMigrateMemObjects(m_h_p_mem, 0); // 0 means from host
 
       // operate on each C_P * F_P corresponding to the same c-dimension indices
       for (size_t f = 0; f < NUM_F_P; ++f) {
@@ -149,10 +164,10 @@ public:
 
         // Launch the systolic array dmmm Kernel, last 2 args always 1
         (*m_krnl_dmmm)(cl::EnqueueArgs(*m_queue, cl::NDRange(1, 1, 1), cl::NDRange(1, 1, 1)),
-                                      h_p_buf, wt_p_buf, *m_hw_p_buf, 1, 1);
+                                      *m_h_p_buf, wt_p_buf, *m_hw_p_buf, 1, 1);
 
         // Copy Result from Device Global Memory to Host Local Memory
-        m_queue->enqueueMigrateMemObjects(m_outBufVec, CL_MIGRATE_MEM_OBJECT_HOST);
+        m_queue->enqueueMigrateMemObjects(m_hw_p_mem, CL_MIGRATE_MEM_OBJECT_HOST);
         m_queue->finish(); // synchronization barrier
 
         accumulate_hw_partition(m_hw_p, hw_r, H_R_ID, f);
@@ -181,10 +196,13 @@ private:
   cl::KernelFunctor<cl::Buffer&, cl::Buffer&, cl::Buffer&, bool, unsigned int>* m_krnl_dmmm;
   float** m_wt_ps;
   cl::Buffer** m_wt_p_bufs;
+  float* m_h_p;   // N_P * C_P
+  cl::Buffer* m_h_p_buf;
+  float* m_hw_p;  // N_P * F_P
   cl::Buffer* m_hw_p_buf;
 
-  std::vector<cl::Memory> m_outBufVec;
-  float m_hw_p[SIZE_HW_P];
+  std::vector<cl::Memory> m_h_p_mem; // for loading altered layer partitions to kernel
+  std::vector<cl::Memory> m_hw_p_mem; // for recieving layer-weight partition product from kernel
 };
 
 
